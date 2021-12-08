2c9137d94974c4d2c2b78a3140a22e7abbfc274a
Kozue Yamada <kozue.yamada@iths.se>
2021-12-08 22:21:41 +0100

    Updated Agency & Booking entities

--- src/main/java/com/example/travelagency/entity/Booking.java
+++ src/main/java/com/example/travelagency/entity/Booking.java
@@ -1,9 +1,6 @@
 package com.example.travelagency.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity(name = "Bookings")
@@ -17,4 +14,60 @@ public class Booking {
     private Long agencyId;
     private double bookingCost;
     private LocalDate bookingDate;
+
+    @ManyToOne
+    private Agency agency;
+
+    @PrePersist
+    public void getCurrentDate() {
+        setBookingDate(LocalDate.now());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Long getAgencyId() {
+        return agencyId;
+    }
+
+    public void setAgencyId(Long agencyId) {
+        this.agencyId = agencyId;
+    }
+
+    public double getBookingCost() {
+        return bookingCost;
+    }
+
+    public void setBookingCost(double bookingCost) {
+        this.bookingCost = bookingCost;
+    }
+
+    public LocalDate getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(LocalDate bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public Agency getAgency() {
+        return agency;
+    }
+
+    public void setAgency(Agency agency) {
+        this.agency = agency;
+    }
 }
