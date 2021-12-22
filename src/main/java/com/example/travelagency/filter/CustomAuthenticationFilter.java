da8ee37a6dae2ffc43de74e48af094d82fc916d6
Sofia Rodriguez <sofsin1208@gmail.com>
2021-12-22 12:14:43 +0100

    Fixed token response

--- src/main/java/com/example/travelagency/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/example/travelagency/filter/CustomAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.example.travelagency.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -16,7 +17,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     public static final Long EXPIRATION_TIME = 10 * 60 * 1000L;
@@ -44,6 +49,9 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
                 .withIssuer(req.getRequestURL().toString())
                 .withClaim("roles", user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(Algorithm.HMAC256("secret".getBytes()));
-        res.setHeader("access_token", access_token);
+        res.setContentType(APPLICATION_JSON_VALUE);
+        Map<String, String> token = new HashMap<>();
+        token.put("access_token", access_token);
+        new ObjectMapper().writeValue(res.getOutputStream(), token);
     }
 }
