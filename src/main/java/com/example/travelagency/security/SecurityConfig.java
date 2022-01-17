4fc967e82adbb3740960a8e49dd32eb3286e4582
sofsin96 <sofsin1208@gmail.com>
2022-01-17 18:50:59 +0100

    Small changes

--- src/main/java/com/example/travelagency/security/SecurityConfig.java
+++ src/main/java/com/example/travelagency/security/SecurityConfig.java
@@ -46,47 +46,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     // Uncomment for ApiWebSecurityConfiguration
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
-        http
-                .authorizeRequests()
-                .antMatchers("/signup", "/h2-console/**").permitAll()
-                .antMatchers(GET, "/users").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/users/edit/{id}", "/users/delete/{id}").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .defaultSuccessUrl("/home", true)
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .and()
-                .logout()
-                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .logoutSuccessUrl("/login");
-    }
-
-    // Uncomment for FormLoginWebSecurityConfiguration
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
 //        http.csrf().disable();
 //        http.headers().frameOptions().disable();
-//        http.sessionManagement().sessionCreationPolicy(STATELESS);
 //        http
 //                .authorizeRequests()
-//                .antMatchers("/login", "/api/v1/users/create", "/h2-console/**").permitAll()
-//                .antMatchers(GET, "/api/v1/users", "/api/v1/users/{id}", "/home").hasAnyRole("USER", "ADMIN")
-//                .antMatchers(POST, "/api/v1/users/add/role", "/api/v1/users/delete/role").hasRole("ADMIN")
-//                .antMatchers(DELETE, "/api/v1/users/{id}").hasRole("ADMIN")
-//                .anyRequest().authenticated();
-//        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
-//        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+//                .antMatchers("/signup", "/h2-console/**").permitAll()
+//                .antMatchers(GET, "/users").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/users/edit/{id}", "/users/delete/{id}").hasRole("ADMIN")
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .defaultSuccessUrl("/home", true)
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .and()
+//                .logout()
+//                .logoutUrl("/logout")
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+//                .clearAuthentication(true)
+//                .invalidateHttpSession(true)
+//                .deleteCookies("JSESSIONID")
+//                .logoutSuccessUrl("/login");
 //    }
+
+    // Uncomment for FormLoginWebSecurityConfiguration
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http
+                .authorizeRequests()
+                .antMatchers("/login", "/api/v1/users/create", "/h2-console/**").permitAll()
+                .antMatchers(GET, "/api/v1/users", "/api/v1/users/{id}", "/home").hasAnyRole("USER", "ADMIN")
+                .antMatchers(POST, "/api/v1/users/add/role", "/api/v1/users/delete/role").hasRole("ADMIN")
+                .antMatchers(DELETE, "/api/v1/users/{id}").hasRole("ADMIN")
+                .anyRequest().authenticated();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
 }
